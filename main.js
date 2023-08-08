fad442b31b774de692227f66a964028ab0254617
Maruk Alena <maruk.lena27@gmail.com>
2023-08-08 13:39:50 +0300

    fix: change index.js file

--- main.js
+++
@@ -1,8 +0,0 @@
-function reverse (n) {
-    let a = String(n);
-   let arr = a.split('');
-   let rev = arr.reverse();
-  let  revStr = rev.join('');
-  let b = Number(revStr);
-  return Math.abs(b);
-}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {  
+    let a = String(n);
+    let arr = a.split('');
+    let rev = arr.reverse();
+    let  revStr = rev.join('');
+   
+    return parseInt(revStr)
 }
